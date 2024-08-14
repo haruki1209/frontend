8cef6447b4eaea804cc8bf78d42ff83a23948de2
Your Name <you@example.com>
2024-08-14 13:26:03 +0900

    frontend

---
+++ src/components/ConfirmationPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmationPage() {
+  return (
+    <div>
+      <h2>確認ページ</h2>
+      <p>フォームが正常に送信されました。</p>
+    </div>
+  );
+}
+
+export default ConfirmationPage;
